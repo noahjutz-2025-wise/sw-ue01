0422ff0797f0585b594dbc6fd1520124a267c9cc
Noah Jutz <noah@noahjutz.com>
2025-10-17 08:27:51 +0200

    Read request params from processform endpoint

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -23,7 +24,8 @@ public class HomeController {
     }
 
     @GetMapping("/processform")
-    public String processform() {
-        return "formprocessed";
+    @ResponseBody
+    public String processform(@RequestParam String username, @RequestParam String password) {
+        return "formprocessed: " + username + ", " + password;
     }
 }
